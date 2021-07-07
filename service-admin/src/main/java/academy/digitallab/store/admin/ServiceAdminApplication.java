3a07a3344c93bc7a4776fed996f407f711324536
Kevin Echeverri <kevin.echeverri@pragma.com.co>
2021-07-07 10:44:51 -0500

    add admin server and actuator

---
+++ service-admin/src/main/java/academy/digitallab/store/admin/ServiceAdminApplication.java
@@ -0,0 +1,16 @@
+package academy.digitallab.store.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class ServiceAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAdminApplication.class, args);
+	}
+
+}
